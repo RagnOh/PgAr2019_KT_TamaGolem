9c625eaeb45524f409f3af68664a87b375c6a53b
Alessandro  Lucchese <alessandrolucchese@MacBook-Pro-di-Alessandro-5.local>
2019-04-23 17:58:00 +0200

    aggiunte classi golem e squadra

--- src/it/PgArnaldo/Tamagolem_KT/Squadra.java
+++ src/it/PgArnaldo/Tamagolem_KT/Squadra.java
@@ -1,5 +1,17 @@
 package it.PgArnaldo.Tamagolem_KT;
+import java.util.ArrayList;
 
-public class Squadra {
+public class Squadra
+{
+  int numGolem;		// numero di golem ammessi per la squadra
+	ArrayList <TamaGolem> squadraGolem = new ArrayList <TamaGolem> ();
 
+	public SquadraTama (int numElementi)
+	{
+		numGolem = ((numElementi-1)*(numElementi-2))/ (2*(((numElementi+1)/3) +1));		// formula per calcolare il numero di Golem
+		for (int i = 0; i< numGolem; i++)	// ciclo per la creazione dei golem -> da define poi
+		{
+
+		}
+	}
 }
--- src/it/PgArnaldo/Tamagolem_KT/Tamagolem.java
+++ src/it/PgArnaldo/Tamagolem_KT/Tamagolem.java
@@ -1,5 +1,44 @@
 package it.PgArnaldo.Tamagolem_KT;
+import java.util.ArrayList;
 
-public class Tamagolem {
+public class Tamagolem
+{
+  int numPietre;			// identifica il numero massimo di pietre che può ingerire il golem
+	ArrayList <Integer> pietre = new ArrayList<Integer>();
+	int vita;
+	int vitaMax;
+	boolean isSulCampo = false;
+	boolean isAlive = true;
 
+	public TamaGolem()	//COSTRUTTORE -> si deve dare: numPietre, vitaMax ,lanciatore
+	{
+
+	}
+
+	public void setVita (int valore)
+	{
+		vita = valore;
+	}
+
+	public void setSulCampo (boolean valore)
+	{
+		isSulCampo = valore;
+	}
+	public void setIsAlive (boolean valore)
+	{
+		isAlive = valore;
+	}
+
+	public boolean getIsAlive ()
+	{
+		return isAlive;
+	}
+	public boolean getSulCampo ()
+	{
+		return isSulCampo;
+	}
+	public int getVita ()
+	{
+		return vita;
+	}
 }
