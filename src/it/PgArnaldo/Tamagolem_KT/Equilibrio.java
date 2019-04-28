f74f4c0ad993097adacb3d9929e99e2c8bb77724
Alessandro  Lucchese <alessandrolucchese@MacBook-Pro-di-Alessandro-5.local>
2019-04-28 17:54:24 +0200

    Inserita classe Equilibrio

--- src/it/PgArnaldo/Tamagolem_KT/Equilibrio.java
+++ src/it/PgArnaldo/Tamagolem_KT/Equilibrio.java
@@ -1,5 +1,65 @@
 package it.PgArnaldo.Tamagolem_KT;
 
-public class Equilibrio {
+import it.unibs.fp.mylib.NumeriCasuali;
 
+public class Equilibrio
+{
+	private final int NUM_ELEMENTI = 6;
+	private int[][] sistemaForze = new int[NUM_ELEMENTI][NUM_ELEMENTI];
+
+	public Equilibrio ()		// serve per riempire la matrice equilibrio con i valori di efficienza dei diversi elementi
+	{
+		int tmp = 0;
+		int danno = 0;
+		int sommaVal = 0;
+		for (int righe = 0; righe < NUM_ELEMENTI; righe++)
+		{
+			sommaVal = 0;
+			for (int colonne = 0; colonne < NUM_ELEMENTI; colonne++)
+			{
+				if (colonne == righe)
+					sistemaForze [righe][colonne] = 0;
+				else if (righe < NUM_ELEMENTI-1 && colonne == NUM_ELEMENTI-1)
+				{
+					sistemaForze [righe][NUM_ELEMENTI-1] = -1*sommaVal;
+					sistemaForze [NUM_ELEMENTI-1][righe] = sommaVal;
+				}
+				else
+				{
+					if (colonne > righe )
+					{
+						tmp = NumeriCasuali.estraiIntero(0, 1);
+						danno = NumeriCasuali.estraiIntero(1, 30);		// numero casuale per l'efficacia dell'elemento
+						if (tmp == 0)
+						{
+							danno *= -1;
+						}
+
+						sistemaForze [righe][colonne] = danno;
+						sistemaForze [colonne][righe] = -1*danno;
+						sommaVal += danno;
+					}
+					else
+					{
+						sommaVal += sistemaForze [righe][colonne];
+					}
+
+				}
+			}	// fine FOR colonne
+		} // fine FOR righe
+
+
+
+	}	// fine COSTRUTTORE
+	public void stampaValori ()
+	{
+		for (int i = 0; i < NUM_ELEMENTI; i++)
+		{
+			for (int j = 0; j < NUM_ELEMENTI; j++)
+			{
+				System.out.print(sistemaForze[i][j]+"  ");
+			}
+			System.out.print("\n");
+		}
+	}
 }
